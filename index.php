788a70cad3fc3be8a7feea16d83f5f32f072dde9
silvanm <silvan.muehlemann@muehlemann-popp.ch>
2011-01-25 13:31:30 +0100

    Check for localhost supports also IPv6 loopback address

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $options = array(
  *
  * Source on Github http://github.com/Seldaek/php-console
  */
-if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+if (!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'), true)) {
     header('HTTP/1.1 401 Access unauthorized');
     die('ERR/401 Go Away');
 }
