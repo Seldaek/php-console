c032079884089c9bc61a00cbdaeb13c251f5ed64
lowrykun <jason@jasonlowry.com>
2015-07-10 15:27:42 +0100

    Added Save and Load to Javascript Functions
    I copied the reset and created load/save.

--- php-console.js
+++ php-console.js
@@ -181,6 +181,54 @@
                 e.preventDefault();
             });
         }
+        
+        //Save
+        if (window.localStorage) {
+            $('.statusbar .save').on('click', function (e) {
+            	var textToWrite = editor.getSession().getValue();
+            	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+            	var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+            	var downloadLink = document.createElement("a");
+            	downloadLink.download = fileNameToSaveAs;
+            	downloadLink.innerHTML = "Download File";
+            	if (window.webkitURL != null)
+            	{
+            		// Chrome allows the link to be clicked
+            		// without actually adding it to the DOM.
+            		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+            	}
+            	else
+            	{
+            		// Firefox requires the link to be added to the DOM
+            		// before it can be clicked.
+            		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            		downloadLink.onclick = destroyClickedElement;
+            		downloadLink.style.display = "none";
+            		document.body.appendChild(downloadLink);
+            	}
+
+            	downloadLink.click();
+            });
+        }
+        
+        //Load
+        if (window.localStorage) {
+            $('.statusbar .load').on('click', function (e) {
+            	var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+            	var fileReader = new FileReader();
+            	fileReader.onload = function(fileLoadedEvent) 
+            	{
+            		var textFromFileLoaded = fileLoadedEvent.target.result;
+            		//document.getElementById("inputTextToSave").value = textFromFileLoaded;
+            		editor.getSession().setValue(textFromFileLoaded);
+            		localStorage.setItem('phpCode',textFromFileLoaded);
+            	};
+            	fileReader.readAsText(fileToLoad, "UTF-8");
+            });
+        }
+        
 
         // commands
         editor.commands.addCommand({
