bfe9a27e72e625c302fbd3f2b1398c48a911e8de
lowrykun <jason@jasonlowry.com>
2015-07-10 23:51:01 +0100

    changing class name per request

--- index.php
+++ index.php
@@ -169,9 +169,9 @@ if (isset($_POST['code'])) {
                                 </object>
                             </span>
                             <a href="" class="reset">Reset</a>
-                            <input type="text" name="inputFileName" id="inputFileNameToSaveAs" class="f" value="<?php echo (isset($_POST['inputFileName']) ? htmlentities($_POST['inputFileName'], ENT_QUOTES, 'UTF-8') : "console-".date("Y-m-d_H:i:s").".php")?>">
+                            <input type="text" name="inputFileName" id="inputFileNameToSaveAs" class="ls-input" value="<?php echo (isset($_POST['inputFileName']) ? htmlentities($_POST['inputFileName'], ENT_QUOTES, 'UTF-8') : "console-".date("Y-m-d_H:i:s").".php")?>">
                             <a href="#" class="save">Save</a>
-                            <input type="file" id="fileToLoad" class="f">
+                            <input type="file" id="fileToLoad" class="ls-input">
                             <a href="#" class="load">Load</a>
                             <span class="runtime-info"></span>
                         </div>
